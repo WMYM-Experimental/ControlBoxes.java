f6c8f7afc9a3be7b6f6adde83a022d095fd33ad7
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2022-04-11 21:54:40 -0700

    Add files via upload

---
+++ homework_stack_2/src/main/java/com/mycompany/homeword_stack_2/MyStack.java
@@ -0,0 +1,48 @@
+package com.mycompany.homeword_stack_2;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author Washington
+ */
+public class MyStack {
+    Stack stk;
+    int maxSize;
+
+
+    public MyStack(int maxSize) {
+        stk = new Stack<Box>();
+        this.maxSize = maxSize; // 6 is default size
+    }
+    
+    public boolean isEmpty(){
+        return this.stk.empty();
+    }
+    
+    public void push(Box box){
+        this.stk.push(box);
+    }
+    
+    public Box pop(){
+        return (Box)this.stk.pop();
+    }
+    
+    public Box peek(){
+        return (Box)this.stk.peek();
+    }
+    
+    public int size(){
+        return this.stk.size();
+    }
+
+    @Override
+    public String toString() {
+        String result = "";
+        for (int i = this.size() - 1; i >= 0 ; i--) {
+            result += this.stk.get(i) + "\n";
+        }
+        return result;
+    }
+    
+}
